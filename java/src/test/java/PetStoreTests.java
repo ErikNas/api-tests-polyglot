15a091f92210a2b15fdcd39c822eabaed399704a
Насибуллин Ирек Рямисович <irek.nasibullin@rt.ru>
2022-12-11 14:42:49 +0300

    add java test

---
+++ java/src/test/java/PetStoreTests.java
@@ -0,0 +1,42 @@
+import io.restassured.http.ContentType;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PetStoreTests {
+
+    @Test
+    void test() {
+        String endpoint = "https://petstore.swagger.io/v2/pet/";
+        String petName = "Барсик";
+
+        JSONObject bodyJO = new JSONObject()
+                .put("name", petName)
+                .put("status", "available");
+
+        String newPetId = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(bodyJO.toString())
+                .post(endpoint)
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getString("id");
+
+        String actualPetName = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .get(endpoint + newPetId)
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getString("name");
+
+        Assertions.assertEquals(petName, actualPetName);
+    }
+}
