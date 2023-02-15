9a5ce0812e652c7a65a46fef2cb5207920330359
Насибуллин Ирек Рямисович <irek.nasibullin@rt.ru>
2023-02-15 16:17:35 +0300

    update rust test

--- rust/tests/pet_store_test.rs
+++ rust/tests/pet_store_test.rs
@@ -1,3 +1,5 @@
+use reqwest::blocking::Client;
+use reqwest::{self, StatusCode, Response};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 
@@ -8,29 +10,29 @@ struct Pet {
 }
 
 #[test]
-fn test_create_new_pet() {
+fn test_create_new_pet() -> Result<(), reqwest::Error> {
     let uri = "https://petstore.swagger.io/v2/pet/";
     let pet_name = "Барсик";
     let body = json!({"name": pet_name, "status": "available"});
 
-    let client = reqwest::blocking::Client::new();
-    
-    let post_res = client.post(uri).json(&body).send().unwrap();
+    let client = Client::new();
 
-    let post_status_code = post_res.status();
-    assert_eq!(post_status_code.as_u16(), 200);
+    let post_res = client.post(uri).json(&body).send()?;
+    assert_eq!(post_res.status(), StatusCode::OK);
 
-    let res_body = post_res.text().unwrap();
-    let root: Value = serde_json::from_str(res_body.as_str()).unwrap();
+    let res_body = post_res.text()?;
+    let root: Value = serde_json::from_str(&res_body).unwrap();
 
     let pet_id = root.get("id");
     let pet_uri = format!("{}{}", uri, pet_id.unwrap());
+    // let pet_uri2 = uri2.push_str(pet_id.unwrap());
 
-    let get_res = client.get(pet_uri).send().unwrap();
+    let get_res = client.get(pet_uri).send()?;
 
-    let get_status_code = get_res.status();
-    assert_eq!(get_status_code.as_u16(), 200);
+    assert_eq!(get_res.status(), StatusCode::OK);
 
-    let actual_pet: Pet = get_res.json().unwrap();
+    let actual_pet: Pet = get_res.json()?;
     assert_eq!(actual_pet.name, pet_name);
+
+    Ok(())
 }
